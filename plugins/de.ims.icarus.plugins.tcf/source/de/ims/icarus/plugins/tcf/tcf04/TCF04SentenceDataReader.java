6a35442cbb47ff20d2b7f29410e00f09c2b5ac35
McGaerty <mcgaerty@ikarus-systems.net>
2013-07-31 15:22:01 +0000

    added legal notice comments on all java files.

--- plugins/de.ims.icarus.plugins.tcf/source/de/ims/icarus/plugins/tcf/tcf04/TCF04SentenceDataReader.java
+++ plugins/de.ims.icarus.plugins.tcf/source/de/ims/icarus/plugins/tcf/tcf04/TCF04SentenceDataReader.java
@@ -1,4 +1,20 @@
 /*
+ *  ICARUS -  Interactive platform for Corpus Analysis and Research tools, University of Stuttgart
+ *  Copyright (C) 2012-2013 Markus Gärtner and Gregor Thiele
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses.
+ *
  * $Revision$
  * $Date$
  * $URL$
@@ -17,7 +33,6 @@ import java.util.EnumSet;
 import java.util.logging.Level;
 
 import de.ims.icarus.language.LanguageConstants;
-import de.ims.icarus.language.LanguageUtils;
 import de.ims.icarus.language.SentenceData;
 import de.ims.icarus.language.SentenceDataReader;
 import de.ims.icarus.language.dependency.DependencyConstants;
